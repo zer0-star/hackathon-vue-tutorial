daa3b83228f27c34564b89a9c1be86a146a9eb28
zer0-star <zer0star.65535@gmail.com>
2024-06-13 18:33:09 +0900

    create

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <main :class="$style.main">
+    <div>
+      <button :class="$style.button" @click="count++">+1</button>
+      <button :class="$style.button" @click="count--">-1</button>
+    </div>
+    <div>count = {{ count }}</div>
+  </main>
+</template>
+
+<style module>
+.main {
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+}
+
+.button {
+  font-size: 20px;
+}
+</style>
