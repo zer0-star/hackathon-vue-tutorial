f5684cb1abcc699bed0393156c9abf9d96348824
zer0-star <zer0star.65535@gmail.com>
2024-06-13 20:01:53 +0900

    add example of snapshot

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
+
+import SnapshotItem from './SnapshotItem.vue'
 
 const count = ref(0)
+const id = ref(1)
+const snapshots: Ref<{ id: number; count: number; time: Date }[]> = ref([])
+
+const addSnapshot = () => {
+  snapshots.value.unshift({
+    id: id.value,
+    count: count.value,
+    time: new Date()
+  })
+
+  id.value++
+}
 </script>
 
 <template>
   <main :class="$style.main">
-    <v-card min-width="300" elevation="4">
+    <v-card width="300" elevation="4">
       <v-card-text>
         <div :class="$style.text">count = {{ count }}</div>
       </v-card-text>
       <v-card-actions>
         <v-btn variant="tonal" density="comfortable" @click="count++">+1</v-btn>
         <v-btn variant="tonal" density="comfortable" @click="count--">-1</v-btn>
+        <v-btn variant="tonal" density="comfortable" @click="addSnapshot">new snapshot</v-btn>
       </v-card-actions>
     </v-card>
+    <div :class="$style.snapshots">
+      <snapshot-item v-for="snapshot in snapshots" :key="snapshot.id" v-bind="snapshot" />
+    </div>
   </main>
 </template>
 
@@ -22,12 +40,20 @@ const count = ref(0)
 .main {
   width: 100%;
   padding: 20px;
-  display: grid;
-  place-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-direction: row;
   font-size: 24px;
+  gap: 8px;
 }
 
 .text {
   font-size: 24px;
+}
+
+.snapshots {
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/SnapshotItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  id: number
+  count: number
+  time: Date
+}>()
+</script>
+
+<template>
+  <v-card width="300" elevation="4">
+    <v-card-title>Snapshot #{{ props.id }}</v-card-title>
+    <v-card-text>
+      <v-list>
+        <v-list-item>count: {{ props.count }}</v-list-item>
+        <v-list-item>time: {{ props.time.toLocaleString('ja-JP') }}</v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
